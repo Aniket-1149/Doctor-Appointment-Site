a6b7abf19ba584d440fc1ed944dceb034ba7c39e
Aniket-1149 <aniketsingh1149@gmail.com>
2025-09-01 01:32:19 +0530

    created user login api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,29 +27,56 @@ const registerUser = async (req, res) => {
         }
 
         //hashing user password
-        const salt= await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password,salt)
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
 
         const userData = {
             name,
             email,
-            password:hashedPassword
+            password: hashedPassword
         }
 
         const newUser = new userModel(userData)
         const user = await newUser.save()
 
-        const token = jwt.sign({id:user._id},process.env.JWT_SECRET )
-  
-        res.json({success:true,token})
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+
+        res.json({ success: true, token })
 
 
 
     } catch (error) {
-       console.log(error);
-        res.json({success:false,message:error.message})
+        console.log(error);
+        res.json({ success: false, message: error.message })
 
     }
 }
 
-export {registerUser}
+//API for user login
+const loginUser = async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body
+        const user = await userModel.findOne({email})
+
+        if (!user) {
+            res.json({ success: false, message: 'user does not exist' })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (isMatch) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            res.json({ success: true, token })
+        }else {
+            res.json({success:false,message:"Invalid credentials"})
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export { registerUser,loginUser }
