84d0b6be3ef8a77f86265781a9485c3582f63498
Aniket-1149 <aniketsingh1149@gmail.com>
2025-09-01 01:59:05 +0530

    connecting login and registration api with frontend

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,14 +9,12 @@ const AppContextProvider=(props)=>{
 
     const currencySymbol='$';
     const backendUrl= import.meta.env.VITE_BACKEND_URL
+
     const [doctors, setDoctors] = useState([])
+    const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
 
 
-    const value={
-        doctors,
-        currencySymbol
-
-    }
+   
 
     const getDoctorsData = async () => {
         
@@ -35,6 +33,14 @@ const AppContextProvider=(props)=>{
             
         }
     } 
+
+     const value={
+        doctors,
+        currencySymbol,
+        token,setToken,
+        backendUrl
+
+    }
 
     useEffect(()=>{
         getDoctorsData()
